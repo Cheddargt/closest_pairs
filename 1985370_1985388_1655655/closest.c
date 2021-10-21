88c9db2a7da8075d0c59271bbcaa30939a1ca816
cheddargt <gus_ghz@live.com>
2021-10-21 12:41:02 -0300

    Trabalho concluído

---
+++ 1985370_1985388_1655655/closest.c
@@ -0,0 +1,218 @@
+/* ALUNOS */
+/* GUSTAVO HENRIQUE ZENI - RA: 1985370 */
+/* IANCA POLIZELO - RA: 1985388 */
+/* VITOR GABRIEL ANDRADE TEIXEIRA - RA: 1655655 */
+
+#include <stdio.h>
+#include <float.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+// Um objeto representando um ponto
+typedef struct
+{
+    double x, y;
+} Ponto;
+
+//Um objeto representando a distância entre dois pontos
+typedef struct
+{
+    Ponto p1, p2;
+    double d;
+}Dist;
+
+int compararX(const void* a, const void* b) {
+    Ponto *p1 = (Ponto *) a;
+    Ponto *p2 = (Ponto *) b;
+    if(p1->x < p2->x) return(-1);
+    if(p1->x <= p2->x+0.0000001 && p1->y <= p2->y) {
+     return (-1);
+    }
+    else return(1);
+}
+
+int compararY(const void* a, const void* b) {
+    Ponto *p1 = (Ponto *) a;
+    Ponto *p2 = (Ponto *) b;
+    if(p1->y < p2->y) return(-1);
+    if(p1->y <= p2->y+0.0000001 && p1->x <= p2->x) {
+     return (-1);
+    }
+    else return(1);
+}
+
+//Função que calcula a distância eucliana entre dois pontos
+Dist distance(Ponto p1, Ponto p2)
+{
+    Dist d;
+    d.d = sqrt(pow((p1.x - p2.x),2)+pow((p1.y - p2.y),2));
+    d.p1 = p1;
+    d.p2 = p2;
+    return(d);
+}
+
+//Função que calcula a menor distância entre um array de pontos
+Dist closest(Ponto X[], Ponto Y[], int tamanho)
+{
+    //Variáveis:
+    Dist d; //É a distância que será retornado
+    Dist d_esq; //Distância do lado esquerdo da divisão
+    Dist d_dir; //Distância do lado direito da divisão
+
+    Ponto mid; //Variável que vai receber o ponto do meio onde faz a divisão pra direita e esquerda
+
+    int meio = tamanho/2; //Metade do vetor
+
+    //Caso base:
+    //Se tiver 2 pontos, apenas retorna a distancia entre eles
+    if(tamanho == 2)
+    {
+        d = distance(X[0],X[1]);
+        return(d);
+    }
+    //Se tiver 3 pontos, calcula a distancia entre os 3 e retorna a menor distancia
+    if(tamanho == 3)
+    {
+        Dist d1 = distance(X[0],X[1]);
+        Dist d2 = distance(X[0],X[2]);
+        Dist d3 = distance(X[1],X[2]);
+
+        if(d1.d<d2.d && d1.d<d3.d) {return(d1);}//quando d1 menor
+
+        if(d2.d<d1.d && d2.d<d3.d) {return(d2);}//quando d2 menor
+
+        //Se chegou até aqui é porque d3 é menor
+        return(d3);
+    }
+
+    //Divisão:
+    Ponto* X_esq = (Ponto*)malloc((meio)*sizeof(Ponto));
+    Ponto* X_dir = (Ponto*)malloc((tamanho-meio)*sizeof(Ponto));
+
+    Ponto* Y_esq = (Ponto*)malloc((meio)*sizeof(Ponto));
+    Ponto* Y_dir = (Ponto*)malloc((tamanho-meio)*sizeof(Ponto));
+
+    mid = X[meio-1];
+
+    for(int i=0; i<meio; i++) {X_esq[i] = X[i];}
+    for(int i=meio; i<tamanho;i++){X_dir[i-meio] = X[i];}
+
+    int tam_esq = 0;
+    int tam_dir = 0;
+
+    for (int i=0; i<tamanho; i++)
+    {
+        if (Y[i].x < mid.x-0.0000001)
+            Y_esq[tam_esq++] = Y[i];
+        else if (Y[i].x<=mid.x+0.0000001 && Y[i].y<mid.y+0.0000001)
+            Y_esq[tam_esq++] = Y[i];
+        else
+            Y_dir[tam_dir++] = Y[i];
+    }
+
+    d_esq = closest(X_esq, Y_esq, tam_esq);//Lado esquerdo
+    d_dir = closest(X_dir, Y_dir, tam_dir);//Lado direito
+
+    //Pega menor distância entre os dois
+    if(d_esq.d < d_dir.d)
+            d = d_esq;
+    else
+            d = d_dir;
+
+    //Combina:
+    Ponto* S = (Ponto*)malloc((tamanho)*sizeof(Ponto)); //Vetor do retangulo
+    int s_size = 0;
+
+    for(int i=0; i<tamanho; i++)
+    {
+        if((Y[i].x >= mid.x-d.d)&&(Y[i].x <=  mid.x+d.d))
+        {
+            S[s_size] = Y[i];
+            s_size++;
+        }
+    }
+
+    for(int i=0; i < s_size; i++)
+    {
+        for(int j=i+1; j < s_size && ((S[j].y - S[i].y) < d.d);j++)
+        {
+            if ((distance(S[i],S[j])).d < d.d)
+            {
+                d = (distance(S[i],S[j]));
+            }
+        }
+    }
+
+    free(X_esq);
+    free(X_dir);
+    free(Y_esq);
+    free(Y_dir);
+    free(S);
+
+    return (d);
+}
+
+
+int main (int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Erro ao ler o arquivo\n");
+        return 0;
+    }
+
+    //Start no tempo de execução
+    clock_t start_closest, end_closest;
+    float cpu_time_clo;
+
+    //Inicia o tempo de execução
+    start_closest = clock();
+
+    Dist menor_dist;
+
+    //Abrir o arquivo
+    FILE *in=fopen(argv[1],"r");
+
+    /* PARA DEBUG */
+    //FILE *in=fopen("input.txt","r");
+
+    int num_pontos = 0;
+
+    fscanf(in, "%d", &num_pontos);;
+    Ponto* pontos_ord_x = (Ponto*)malloc((num_pontos)*sizeof(Ponto));
+    Ponto* pontos_ord_y = (Ponto*)malloc((num_pontos)*sizeof(Ponto));
+
+    for (int i = 0; i < num_pontos; i++)
+    {
+        double x, y;
+        fscanf(in, "%lf %lf", &x, &y);
+        pontos_ord_x[i].x = x;
+        pontos_ord_x[i].y = y;
+        pontos_ord_y[i].x = x;
+        pontos_ord_y[i].y = y;
+    }
+
+    //Fechar o arquivo
+    fclose(in);
+
+    //Ordena os pontos pela coordenada x e salva em um vetor X
+    qsort(pontos_ord_x, num_pontos, sizeof(Ponto), compararX);
+
+    //Ordena os pontos pela coordenada y e salva em um vetor Y
+    qsort(pontos_ord_y, num_pontos, sizeof(Ponto), compararY);
+
+    //Chama a função que pega a menor distância
+    menor_dist = closest(pontos_ord_x, pontos_ord_y, num_pontos);
+
+    //Parar tempo de execução
+    end_closest = clock();
+    cpu_time_clo = ((float) (end_closest - start_closest)) / CLOCKS_PER_SEC;
+
+    //Printa saída (tempo distância x1 y1 x2 y2)
+    printf("%f %lf %lf %lf %lf %lf\n", cpu_time_clo, menor_dist.d, menor_dist.p1.x, menor_dist.p1.y, menor_dist.p2.x, menor_dist.p2.y);
+
+    free(pontos_ord_x);
+    free(pontos_ord_y);
+
+    return 0;
+}
--- closest.c
+++ closest.c
@@ -9,23 +9,17 @@
 #include <math.h>
 #include <time.h>
 
-// TODO: ler o input.txt
-
-// criar um vetor de pontos
-// adicionar todos os pontos ao vetor
-// ordenar usando merge-sort
-
 // Um objeto representando um ponto
 typedef struct
 {
-    float x, y;
+    double x, y;
 } Ponto;
 
 //Um objeto representando a distância entre dois pontos
 typedef struct
 {
     Ponto p1, p2;
-    float d;
+    double d;
 }Dist;
 
 int compararX(const void* a, const void* b) {
@@ -42,8 +36,10 @@ int compararY(const void* a, const void* b) {
     Ponto *p1 = (Ponto *) a;
     Ponto *p2 = (Ponto *) b;
     if(p1->y < p2->y) return(-1);
-    if(p1->y == p2->y) return(0);
-    if(p1->y > p2->y) return(1);
+    if(p1->y <= p2->y+0.0000001 && p1->x <= p2->x) {
+     return (-1);
+    }
+    else return(1);
 }
 
 //Função que calcula a distância eucliana entre dois pontos
@@ -59,11 +55,6 @@ Dist distance(Ponto p1, Ponto p2)
 //Função que calcula a menor distância entre um array de pontos
 Dist closest(Ponto X[], Ponto Y[], int tamanho)
 {
-
-    if (tamanho == 1) {
-        getch();
-    }
-
     //Variáveis:
     Dist d; //É a distância que será retornado
     Dist d_esq; //Distância do lado esquerdo da divisão
@@ -72,7 +63,6 @@ Dist closest(Ponto X[], Ponto Y[], int tamanho)
     Ponto mid; //Variável que vai receber o ponto do meio onde faz a divisão pra direita e esquerda
 
     int meio = tamanho/2; //Metade do vetor
-    //int impar = 0; //Flag pra saber se tamanho é ímpar
 
     //Caso base:
     //Se tiver 2 pontos, apenas retorna a distancia entre eles
@@ -97,12 +87,11 @@ Dist closest(Ponto X[], Ponto Y[], int tamanho)
     }
 
     //Divisão:
+    Ponto* X_esq = (Ponto*)malloc((meio)*sizeof(Ponto));
+    Ponto* X_dir = (Ponto*)malloc((tamanho-meio)*sizeof(Ponto));
 
-    Ponto X_esq[meio];
-    Ponto X_dir[tamanho-meio];
-
-    Ponto Y_esq[meio];
-    Ponto Y_dir[tamanho-meio];
+    Ponto* Y_esq = (Ponto*)malloc((meio)*sizeof(Ponto));
+    Ponto* Y_dir = (Ponto*)malloc((tamanho-meio)*sizeof(Ponto));
 
     mid = X[meio-1];
 
@@ -116,32 +105,23 @@ Dist closest(Ponto X[], Ponto Y[], int tamanho)
     {
         if (Y[i].x < mid.x-0.0000001)
             Y_esq[tam_esq++] = Y[i];
-        else if (Y[i].x<=mid.x+0.0000001 && Y[i].y<mid.y-0.0000001)
+        else if (Y[i].x<=mid.x+0.0000001 && Y[i].y<mid.y+0.0000001)
             Y_esq[tam_esq++] = Y[i];
         else
             Y_dir[tam_dir++] = Y[i];
     }
 
-
-    if (tam_dir == 1 || tam_esq == 1) {
-        printf("erro");
-    }
     d_esq = closest(X_esq, Y_esq, tam_esq);//Lado esquerdo
     d_dir = closest(X_dir, Y_dir, tam_dir);//Lado direito
 
-
-
     //Pega menor distância entre os dois
     if(d_esq.d < d_dir.d)
-        {
             d = d_esq;
-    }
-    else {
+    else
             d = d_dir;
-    }
 
     //Combina:
-    Ponto S[tamanho]; //Vetor do retangulo
+    Ponto* S = (Ponto*)malloc((tamanho)*sizeof(Ponto)); //Vetor do retangulo
     int s_size = 0;
 
     for(int i=0; i<tamanho; i++)
@@ -164,49 +144,58 @@ Dist closest(Ponto X[], Ponto Y[], int tamanho)
         }
     }
 
+    free(X_esq);
+    free(X_dir);
+    free(Y_esq);
+    free(Y_dir);
+    free(S);
+
     return (d);
 }
 
+
 int main (int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Erro ao ler o arquivo\n");
+        return 0;
+    }
 
     //Start no tempo de execução
     clock_t start_closest, end_closest;
-    clock_t start_brute, end_brute;
-    float cpu_time_clo, cpu_time_bru;
-
-    Dist menor_dist;
-
-    FILE *in=fopen(argv[1],"r");
-
-    // ler linha por linha, máximo 256 bytes
-    const unsigned MAX_LENGTH = 256;
-    char buffer[MAX_LENGTH];
-    int linha = 0;
-    int num_pontos = 0;
-
-    num_pontos = atoi(fgets(buffer, MAX_LENGTH, in));
-    Ponto pontos_ord_x[num_pontos];
-    Ponto pontos_ord_y[num_pontos];
-
-    while (fgets(buffer, MAX_LENGTH, in)) {
-        char *xcoord = strtok(buffer, " ");
-        pontos_ord_x[linha].x = atof(xcoord);
-        pontos_ord_y[linha].x = atof(xcoord);
-        xcoord = strtok(NULL, " ");
-        char *ycoord = strtok(xcoord, "\n");
-        pontos_ord_x[linha].y = atof(ycoord);
-        pontos_ord_y[linha].y = atof(ycoord);
-        linha++;
-    }
-
-    // fechar o arquivo
-    fclose(in);
+    float cpu_time_clo;
 
     //Inicia o tempo de execução
     start_closest = clock();
 
+    Dist menor_dist;
+
+    //Abrir o arquivo
+    FILE *in=fopen(argv[1],"r");
+
+    /* PARA DEBUG */
+    //FILE *in=fopen("input.txt","r");
+
+    int num_pontos = 0;
+
+    fscanf(in, "%d", &num_pontos);;
+    Ponto* pontos_ord_x = (Ponto*)malloc((num_pontos)*sizeof(Ponto));
+    Ponto* pontos_ord_y = (Ponto*)malloc((num_pontos)*sizeof(Ponto));
+
+    for (int i = 0; i < num_pontos; i++)
+    {
+        double x, y;
+        fscanf(in, "%lf %lf", &x, &y);
+        pontos_ord_x[i].x = x;
+        pontos_ord_x[i].y = y;
+        pontos_ord_y[i].x = x;
+        pontos_ord_y[i].y = y;
+    }
+
+    //Fechar o arquivo
+    fclose(in);
+
     //Ordena os pontos pela coordenada x e salva em um vetor X
-    //qsort(pontos_ord_x, num_pontos, sizeof(Ponto), compararY);
     qsort(pontos_ord_x, num_pontos, sizeof(Ponto), compararX);
 
     //Ordena os pontos pela coordenada y e salva em um vetor Y
@@ -219,9 +208,11 @@ int main (int argc, char *argv[]) {
     end_closest = clock();
     cpu_time_clo = ((float) (end_closest - start_closest)) / CLOCKS_PER_SEC;
 
-
     //Printa saída (tempo distância x1 y1 x2 y2)
-    printf("%f %f %f %f %f %f\n", cpu_time_clo, menor_dist.d, menor_dist.p1.x, menor_dist.p1.y, menor_dist.p2.x, menor_dist.p2.y);
+    printf("%f %lf %lf %lf %lf %lf\n", cpu_time_clo, menor_dist.d, menor_dist.p1.x, menor_dist.p1.y, menor_dist.p2.x, menor_dist.p2.y);
+
+    free(pontos_ord_x);
+    free(pontos_ord_y);
 
     return 0;
 }
